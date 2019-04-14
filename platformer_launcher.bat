e21fdcb01f6cf97031e4a55c0eac2c159f467e03
pyamedee <leberre.amedee@gmail.com>
2019-04-14 16:23:49 +0200

    Ajouts mineurs

--- platformer_launcher.bat
+++ platformer_launcher.bat
@@ -1,5 +1,3 @@
 @echo off
-call "%~dp0\venv\Scripts\activate.bat"
-set "PYTHONPATH=%~dp0;%PYTHONPATH%"
-start "%~dp0\venv\Scripts\pythonw.exe" "%~dp0\__main__.pyw"
-call "%~dp0\venv\Scripts\deactivate.bat"
+call "%~dp0\activate.cmd"
+start "%base%\venv\Scripts\pythonw.exe" "%base%\__main__.pyw"
